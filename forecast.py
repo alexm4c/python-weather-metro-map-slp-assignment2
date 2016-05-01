b7de1942ecce2a41005dbeeff7799ddff7475bad
Alex McBain <alex@mcbain.net.au>
2016-05-01 17:30:52 +1000

    weather scratches

---
+++ forecast.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import urllib2
+import json
+
+API_URL = "https://api.forecast.io/forecast/"
+API_KEY = "3263712d1c1452735faa19a7f9b90edc"
+OPTIONS = "?units=si&exclude=minutely,hourly,daily,alerts,flags"
+
+def call_forecast_api(latitude, longitude):
+	
+	# build up api call url
+	url =  API_URL 
+	url += API_KEY 
+	url += "/"
+	url += latitude 
+	url += "," 
+	url += longitude 
+	url += OPTIONS
+
+	# make the api call and store returned json data
+	forecast_json = urllib2.urlopen(url).read()
+	# convert json to dictionary
+	forecast = json.loads(forecast_json)
+
+	return forecast
+
+### end of get_forecast ###
---
+++ stage1.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+
+import sys
+from stops import csv_extract_list, validate_stop_name
+from forecast import call_forecast_api
+
+STOPS_PATH = "stops.txt"
+
+stop_name = sys.argv[1]
+
+stops_list = csv_extract_list(STOPS_PATH)
+valid_stops = validate_stop_name(stop_name, stops_list)
+
+if len(valid_stops) < 1:
+	print "No match for stop name: " + stop_name
+	exit()
+elif len(valid_stops) > 1:
+	print "Ambiguous stop name, which did you mean?"
+	for index, stop in enumerate(valid_stops):
+		print "[" + str(index) + "] " + stop["stop_name"]
+
+	response = int(raw_input(": "))
+
+	if response >= 0 and response < len(valid_stops):
+		valid_stop = valid_stops[response]
+	else:
+		print "You are beautiful!"
+		exit()
+else:
+	valid_stop = valid_stops[0]
+
+
+# Call the forecast api
+forecast = call_forecast_api(valid_stop["stop_lat"], valid_stop["stop_lon"])
+
+
+## We need to prettify the forecast data into human readable output
+
+# Assign variable names for readability
+temperature = forecast["currently"]["temperature"]
+rain_chance = forecast["currently"]["precipProbability"]
+rain_intensity = forecast["currently"]["precipIntensity"]
+wind_speed = forecast["currently"]["windSpeed"]
+wind_direction = forecast["currently"]["windBearing"]
+
+## Now build strings for printing
+## Rain amount and wind direction are a bit tricky so let's do them first
+
+# Convert rain amount in mm/hr into a classification of:
+# None, Light, Moderate, Heavy
+if rain_intensity < 0.05:
+	rain_intensity_str = "None"
+elif rain_intensity < 0.5:
+	rain_intensity_str = "Light"
+elif rain_intensity < 2.5:
+	rain_intensity_str = "Moderate"
+else: #rain intensity is >2.5
+	rain_intensity_str = "Heavy"
+
+# Convert wind direction in degrees to compass directions
+# N, NE, E, SE, S, SW, W, NW
+# 0, 45, 90, 135, 180, 225, 270, 315
+# The final string is determined by 45 degrees wedges, or,
+# 22.5 degrees either side of each exact direction
+# I hope that made sense, else look at a compass
+
+if wind_direction == None:
+	#API Docs says if wind dir is 0, it isn't even returned in the api call
+	wind_direction_str = "N"
+elif wind_direction > 337.5 or wind_direction <= 22.5:
+	wind_direction_str = "N"
+elif wind_direction <= 67.5:
+	wind_direction_str = "NE"
+elif wind_direction <= 112.5:
+	wind_direction_str = "E"
+elif wind_direction <= 157.5:
+	wind_direction_str = "SE"
+elif wind_direction <= 202.5:
+	wind_direction_str = "S"
+elif wind_direction <= 247.5:
+	wind_direction_str = "SW"
+elif wind_direction <= 292.5:
+	wind_direction_str = "W"
+elif wind_direction <= 337.5:
+	wind_direction_str = "NW"
+else:
+	# Something bad happened
+	wind_direction_str = "Error"
+
+# Convert rain chance from decimal to percentage value
+rain_chance = int(rain_chance * 100)
+
+
+## Finally print everything out
+
+# Print temperature
+print "Temp - celsius: %0.2f" % (temperature)
+
+# Print rain
+print "Rain - chance: %d%%, intensity: %s" % (rain_chance, rain_intensity_str) 
+
+# Print wind
+print "Wind - speed: %0.2fm/s, direction: %s" % (wind_speed, wind_direction_str)
